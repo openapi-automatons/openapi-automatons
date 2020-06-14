2447ca89db64bbadb993329663613e6957b3e51a
tanmen <yt.prog@gmail.com>
2020-06-15 02:03:02 +0900

    fix: include templates

--- packages/@automatons/typescript/clients/axios/scripts/prebuild.js
+++ packages/@automatons/typescript/clients/axios/scripts/prebuild.js
@@ -1,6 +1,4 @@
 const fs = require('fs-extra');
-const path = require('path');
 const paths = require('../config/paths');
 
 fs.removeSync(paths.build);
-fs.copySync(paths.templates, path.resolve(paths.build, 'templates'), {dereference: true});
