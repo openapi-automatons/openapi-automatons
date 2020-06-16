ef3313f54c8cec1a5310c601c5c04d3e3a5cdb1a
tanmen <yt.prog@gmail.com>
2020-06-16 16:11:56 +0900

    fix: pascal to camel

--- packages/@automatons/typescript/parser/src/converters/security.ts
+++ packages/@automatons/typescript/parser/src/converters/security.ts
@@ -1,16 +1,16 @@
 import {Security} from "../types";
 import {OpenapiSecuritySchema} from "@automatons/tools/dist";
-import { pascalCase } from "change-case";
+import { camelCase } from "change-case";
 
 export const convertSecurity = (name: string, security: OpenapiSecuritySchema, scopes: string[]): Security | undefined => {
   switch (security.type) {
     case 'oauth2':
-      return {...security, name: pascalCase(name), scopes};
+      return {...security, name: camelCase(name), scopes};
     case 'apiKey':
-      return {...security, name: pascalCase(name), key: security.name}
+      return {...security, name: camelCase(name), key: security.name}
     case 'http':
     case 'openIdConnect':
-      return {...security, name: pascalCase(name)}
+      return {...security, name: camelCase(name)}
     default:
       return undefined;
   }
